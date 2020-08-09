c15cc3206d65755af9be0d4a17318f6df4675538
Bailey Jin <baileyjin97@gmail.com>
2020-08-09 15:45:43 +1000

    Include some comments to document and explain functions

--- clock_timestamp_controller.py
+++ clock_timestamp_controller.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import clock_timestamp_model
 from clock_timestamp_model import Model
 import tkinter as tk
 from tkinter import ttk, messagebox
@@ -48,13 +47,14 @@ class Controller:
 
         self.root.mainloop()
 
+    # EVENT TRIGGER TO UPDATE WINDOW SIZE FOR EACH NOTEBOOK APP
     def _on_tab_changed(self, event):
         # gets the widget clicked and configures the height and width.
         event.widget.update_idletasks()
         tab = event.widget.nametowidget(event.widget.select())
         event.widget.configure(height=tab.winfo_reqheight(), width=tab.winfo_reqwidth()+30)
-        # Updates tab accordingly
 
+        # UPDATES EACH TAB'S CONTENT WHEN CLICKED ON
         selected_tab = event.widget.tab(event.widget.select(), 'text')
         if selected_tab == 'Clock On':
             if self.clock_on_app.employee:
@@ -100,12 +100,12 @@ class Controller:
                 self.display_timesheet_grid(self.model.get_current_date(), 7)
         return 'break'
 
-
     # EMPLOYEE LIST EVENT CALL FOR WHEN AN EMPLOYEE IS SELECTED FROM THE LIST
     def alter_emp_select(self, event):
         self.alter_hour_app.on_combo_select()
         self.update_show_time()
 
+    # EVENT CALL FOR WHEN A DATE IS SELECTED IN THE ALTER HOUR APP
     def alter_date_select(self, event):
         self.alter_hour_app.on_date_select()
         for time_type in ['clock_on', 'clock_off']:
@@ -140,37 +140,52 @@ class Controller:
             elif time_type == 'clock_off':
                 self.alter_hour_app.off_button['text'] = 'None'
 
+    # SET ALTERED TIME RECORD
     def alter_time(self, time_type):
         emp_id = self.model.get_id_by_name(self.alter_hour_app.employee)
         clock_off_time = self.model.get_time('clock_off', emp_id, self.alter_hour_app.date_value)
         clock_on_time = self.model.get_time('clock_on', emp_id, self.alter_hour_app.date_value)
+
         try:
-            if self.alter_hour_app.employee:
-                if self.alter_hour_app.ask_password():
-                    self.alter_hour_app.alter_time()
-                    if self.alter_hour_app.time_value:
-                        if clock_on_time or clock_off_time:
-                            if time_type == 'clock_on':
-                                if clock_off_time:
-                                    if self.alter_hour_app.time_value <= clock_off_time:
-                                        self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
-                                    else:
-                                        raise excep.IllogicalTime
-                                else:
-                                    self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
-                            elif time_type == 'clock_off':
-                                if clock_on_time:
-                                    if clock_on_time <= self.alter_hour_app.time_value:
-                                        self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
-                                    else:
-                                        raise excep.IllogicalTime
-                                else:
-                                    self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
-                        else:
-                            self.model.create_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
-                        self.update_show_time()
-            else:
+            # Throw error if user not selected
+            if not self.alter_hour_app.employee:
                 raise excep.NoEmployeeSelected
+            # Throw error if password is incorrect
+            if not self.alter_hour_app.ask_password():
+                raise excep.IncorrectPassword
+            # Launch alter time dialog if no errors
+            self.alter_hour_app.alter_time()
+            # Continue if cancel is not clicked
+            if self.alter_hour_app.time_value:
+                # Run if either clock on/off time exists
+                if clock_on_time or clock_off_time:
+                    # If the user selects clock on time to be altered
+                    if time_type == 'clock_on':
+                        # Checks if clock off time exists
+                        if clock_off_time:
+                            # If clock off exists, check if the time selected (clock on) is less than clock off
+                            if self.alter_hour_app.time_value <= clock_off_time:
+                                self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+                            else:
+                                raise excep.IllogicalTime
+                        # If clock off time does not exist, no need to validate.
+                        else:
+                            self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+                    # Handles clock off selection.
+                    elif time_type == 'clock_off':
+                        if clock_on_time:
+                            if clock_on_time <= self.alter_hour_app.time_value:
+                                self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+                            else:
+                                raise excep.IllogicalTime
+                        else:
+                            self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+                # If both clock on and clock off time does not exist, allow admin to immediately create record.
+                else:
+                    self.model.create_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+                # Update the labels accordingly.
+                self.update_show_time()
+
         except excep.IncorrectPassword:
             messagebox.showerror('Access denied', 'Incorrect password')
             return 'break'
@@ -182,6 +197,53 @@ class Controller:
         except excep.NoEmployeeSelected:
             messagebox.showinfo('Attention', 'No Employee seleceted')
             return 'break'
+
+
+
+    # SETTING OF TIME RECORD
+    # def alter_time(self, time_type):
+    #     emp_id = self.model.get_id_by_name(self.alter_hour_app.employee)
+    #     clock_off_time = self.model.get_time('clock_off', emp_id, self.alter_hour_app.date_value)
+    #     clock_on_time = self.model.get_time('clock_on', emp_id, self.alter_hour_app.date_value)
+    #     try:
+    #         # Error if user not selected
+    #         if self.alter_hour_app.employee:
+    #             if self.alter_hour_app.ask_password():
+    #                 self.alter_hour_app.alter_time()
+    #                 if self.alter_hour_app.time_value:
+    #                     if clock_on_time or clock_off_time:
+    #                         if time_type == 'clock_on':
+    #                             if clock_off_time:
+    #                                 if self.alter_hour_app.time_value <= clock_off_time:
+    #                                     self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+    #                                 else:
+    #                                     raise excep.IllogicalTime
+    #                             else:
+    #                                 self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+    #                         elif time_type == 'clock_off':
+    #                             if clock_on_time:
+    #                                 if clock_on_time <= self.alter_hour_app.time_value:
+    #                                     self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+    #                                 else:
+    #                                     raise excep.IllogicalTime
+    #                             else:
+    #                                 self.model.set_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+    #                     else:
+    #                         self.model.create_time_record(time_type, emp_id, self.alter_hour_app.date_value, self.alter_hour_app.time_value)
+    #                     self.update_show_time()
+    #         else:
+    #             raise excep.NoEmployeeSelected
+    #     except excep.IncorrectPassword:
+    #         messagebox.showerror('Access denied', 'Incorrect password')
+    #         return 'break'
+    #
+    #     except excep.IllogicalTime:
+    #         messagebox.showinfo('Attention', 'Clock ON time must be less than Clock OFF time')
+    #         return 'break'
+    #
+    #     except excep.NoEmployeeSelected:
+    #         messagebox.showinfo('Attention', 'No Employee seleceted')
+    #         return 'break'
 
     # DATE SELECTOR FOR TIME SHEET APP EVENT CALL
     def date_select(self, event):
